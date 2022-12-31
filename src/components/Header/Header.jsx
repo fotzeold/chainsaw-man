abac701c562f04aae1c8c412e9bcda48bfdd451e
Roman Popovych <mechenuyzzzz@gmail.com>
2022-12-31 10:32:23 +0200

    Add Header component

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,52 +1,81 @@
+import { useState, useEffect } from "react";
+import CharacterServices from "../../services/CharacterServices";
 import './header.scss';
 
 const Header = () => {
+	const [valueInp, setValueInp] = useState(null);
+	const [searchedChar, setSearchedChar] = useState([]);
+
+	const { characters } = CharacterServices();
+
+	useEffect(() => onSearched(), [searchedChar])
+
+	const onSearched = () => {
+		setSearchedChar(characters.filter(e => e.name.includes(valueInp)));
+	}
 
 	const togglerBurger = () => {
-		document.querySelector(".menu").classList.toggle("change");
+		document.querySelector(".menu").classList.toggle("active-menu");
 		document.querySelector(".menu-bar").classList.toggle("change-bar");
 		document.body.classList.toggle("overflow-hidden");
 	}
 
 	const removeBurger = () => {
-		document.querySelector(".menu").classList.remove("change");
+		document.querySelector(".menu").classList.remove("active-menu");
 		document.querySelector(".menu-bar").classList.remove("change-bar");
 		document.body.classList.remove("overflow-hidden");
 	}
 
 	return (
-		<header>
-			<div className="header-row">
-				<div className="menu">
-					<nav className="nav" id="nav">
-						<ul>
-							<li className="p-2">
-								<a
-									onClick={removeBurger}
-								>Main
-								</a>
-							</li>
-							<li className="p-2">
-								<a
-									onClick={removeBurger}
-								>Movies
-								</a>
-							</li>
-						</ul>
-					</nav>
-				</div>
-				<div className="burger">
-					<div className="menu-bar" onClick={togglerBurger}>
-						<div id="bar1" className="bar"></div>
-						<div id="bar2" className="bar"></div>
-						<div id="bar3" className="bar"></div>
+		<>
+			<header>
+				<div className="header-row">
+					<div className="burger">
+						<div className="menu-bar" onClick={togglerBurger}>
+							<div id="bar1" className="bar avtive"></div>
+							<div id="bar2" className="bar avtive"></div>
+							<div id="bar3" className="bar avtive"></div>
+						</div>
+					</div>
+					<div className="menu">
+						<nav className="nav" id="nav">
+							<ul>
+								<li className="p-2">
+									<a
+										onClick={removeBurger}
+									>Characters
+									</a>
+								</li>
+								<li className="p-2">
+									<a
+										onClick={removeBurger}
+									>Movies
+									</a>
+								</li>
+							</ul>
+						</nav>
+					</div>
+					<div className="search">
+						<input
+							type="text"
+							placeholder='Find you hero'
+							onChange={(e) => setValueInp(e.target.value)}
+						/>
 					</div>
 				</div>
-				<div className="search">
-					<input type="text" placeholder='Find you hero' />
-				</div>
+			</header >
+			<div className="searched-content">
+				{searchedChar.length > 0 ?
+					searchedChar.map(e => {
+						return (
+							<div className="hero">
+								<h4>{e.name}</h4>
+							</div>
+						)
+					})
+					: null}
 			</div>
-		</header >
+		</>
 	)
 }
 
